b706853b159444fa768f5960d3e00f2c82cd3fb9
QRUMN <quorumnova@gmail.com>
2024-12-30 18:10:02 -0500

    fix: Improve build process
    - Explicitly install Vite and React plugin
    - Add TypeScript check to build
    - Update Vite config typing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import { UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default (): UserConfig => ({
   plugins: [react()],
   build: {
     outDir: 'dist',
